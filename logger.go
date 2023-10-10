4eacfc18f395463fc6767260691563ffe47edf3d
Jason Hancock <jason@jasonhancock.com>
2023-10-10 16:46:30 -0700

    update logger interface

--- logger.go
+++ logger.go
@@ -2,5 +2,5 @@ package backoff
 
 // Logger is the interface the Backoff will accept for logging messages.
 type Logger interface {
-	Warn(any string, keyvals ...interface{})
+	Warn(msg any, keyvals ...any)
 }
