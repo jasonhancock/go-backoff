d4766ed7dc741199f7c78e810b14a2c29253e54c
Jason Hancock <jason@jasonhancock.com>
2024-09-10 12:03:07 -0700

    add non-retryable error wrapper

--- error.go
+++ error.go
@@ -7,3 +7,32 @@ package backoff
 type Retrier interface {
 	Retry() bool
 }
+
+// NonRetryableError wraps an err to add a Retry() method that conforms to
+// Retrier interface which then prevents a backoff runner from retrying.
+type NonRetryableError struct {
+	err error
+}
+
+// Unwrap returns the underlying wrapped error.
+func (e NonRetryableError) Unwrap() error {
+	return e.err
+}
+
+// Error returns the error as a string.
+func (e NonRetryableError) Error() string {
+	return e.err.Error()
+}
+
+// Retry tells the backoff not to retry this error.
+func (e NonRetryableError) Retry() bool {
+	return false
+}
+
+// NewNonRetryable initializes a non retryable error.
+func NewNonRetryable(err error) error {
+	if err == nil {
+		return nil
+	}
+	return &NonRetryableError{err: err}
+}
